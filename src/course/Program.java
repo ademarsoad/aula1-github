d8dd092161c1a5edbd5c7a7178bac03968d0522c
Ademarsoad <ademarsoad@gmail.com>
2021-05-17 12:47:45 -0300

    Acrescentar digitar o nome

--- src/course/Program.java
+++ src/course/Program.java
@@ -1,9 +1,14 @@
 package course;
 
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("Ademar Couto Guimaraes");
+		Scanner sc = new Scanner(System.in);
+		
+		String nome = sc.nextLine();
+		
+		System.out.println(nome);
 	}
-
 }
